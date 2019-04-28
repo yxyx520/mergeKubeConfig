9a87e8a8497bd95256cc1bcf9e194f875c30be39
郭旭东 <guoxudong@keking.cn>
2019-04-28 11:25:53 +0800

    新增功能

--- README.md
+++ README.md
@@ -4,14 +4,21 @@
 
 * 需要在终端使用命令行管理多集群
 * kubernetes集群中安装了istio，需要使用```istioctl```命令，但是集群节点并没有安装```istioctl```，需要在本地终端操作
-* 不愿频繁编辑.kube目录中的config文件的同学
+* 不愿频繁编辑 ```.kube``` 目录中的config文件的同学
+
+## 2019-04-28 更新内容
+新增增量合并，在已使用本脚本合并后的 kubeconfig 后，可以使用 ```-a addfilename -t tofilename``` 参数将新的配置合并到原来已经合并好的文件中
+
+## 注意事项
+* 使用本脚本合并前，请先备份您您的配置文件，以免发生意外
+* 使用增量添加时，默认新增文件在```merge.py```同级目录，使用相对路径
 
 ## 准备工作
 
 * Python环境：2.7或者3均可
 * 需要依赖包：```PyYAML```
 
-## 开始使用
+## 快速使用
 
 * 安装依赖：
 
@@ -26,6 +33,10 @@
     * 自定义kubeconfig文件目录
     
             python merge.py -d {custom-dir}
+            
+    * 向已合并的配置文件中增加新的配置，这里默认新增文件在```merge.py```同级目录，使用相对路径
+    
+            python merge.py -a addfilename -t tofilename
             
 ## 运行后操作
 
@@ -44,3 +55,13 @@
 * 切换kubernetes配置
 
         kubectl config use-context {your-contexts}
+        
+## 问题反馈
+
+* 直接提 issue
+* 个人博客 https://log.maoxianplay.com 上面有联系方式
+* 邮箱： sunnydog0826@gmail.com
+
+## 代码许可
+
+MIT许可证，参见LICENSE文件。
--- merge.py
+++ merge.py
@@ -4,6 +4,7 @@
 """
 Author: guoxudong
 Time: 2019-03-15
+UpdateTime: 2019-04-28
 
 This script is used to merge the file of kubeconfig
 Before using it, you should make sure your python has package: PyYAML
@@ -13,6 +14,9 @@ Install it: pip install PyYAML
 这个脚本用于合并多个kubeconfig文件，用于在一台终端操作多个kubernetes集群
 需要提前安装的Python包：PyYAML
 安装：pip install PyYAML
+
+2019-04-28 更新内容：
+新增增量合并，在已使用本合并后的 kubeconfig 后，可以使用 ```-a addfilename -t tofilename``` 参数将新的配置合并到原来已经合并好的文件中
 
 ---
 Before Usage:
@@ -29,10 +33,16 @@ Usage:
   Select directory:
     python merge.py -d configfile
 
+  add new config:
+    python merge.py -a addfilename -t tofilename
+
 ---
 使用：
   自定义选择目录
     python merge.py -d configfile
+
+  向已合并的配置文件中增加新的配置
+    python merge.py -a addfilename -t tofilename
 
 """
 import argparse
@@ -56,24 +66,114 @@ KUBECONFIG = {
 ALLCONTEXTS = []
 
 
-def getLocalFiles():
+class ToException(Exception):
+    def __init__(self, err='请选择源配置文件'):
+        Exception.__init__(self, err)
+
+
+class AddException(Exception):
+    def __init__(self, err='请选择配置文件'):
+        Exception.__init__(self, err)
+
+
+def getLocalDirectory():
     """
-    Process local files
-    处理本地文件
+    Process local directory
+    获取目录地址
     :param dir:
-    :return: 配置文件路径
+    :return: 配置文件目录路径
     """
     return os.path.dirname(os.path.realpath(__file__)) + '/' + FLAGS.directory + '/'
 
 
+def getLocalFile(filename):
+    """
+    Process local files
+    获取文件地址
+    :param dir:
+    :return: 配置文件路径
+    """
+    return os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+
+
+def addConfig():
+    """
+    增加配置文件
+    :return:
+    """
+    add_path = getLocalFile(FLAGS.add)
+    to_path = getLocalFile(FLAGS.to)
+    handleAddConfig(add_path, to_path)
+
+
+def loadYamlFile(path):
+    """
+    读取yaml文件
+    :param path:
+    :return:
+    """
+    with open(path, 'r') as f:
+        return yaml.load(f.read(), Loader=yaml.FullLoader)
+
+
+def handleAddConfig(addfile, oldconfig):
+    """
+    处理增加配置
+    :return:
+    """
+    add_config = loadYamlFile(addfile)
+    to_config = loadYamlFile(oldconfig)
+    add_content = handleYaml(add_config, FLAGS.add.split('/')[-1])
+    to_config['clusters'].extend(add_content.get('clusters', ''))
+    to_config['users'].extend(add_content.get('users', ''))
+    to_config['contexts'].extend(add_content.get('contexts', ''))
+    KUBECONFIG['clusters'] = to_config['clusters']
+    KUBECONFIG['users'] = to_config['users']
+    KUBECONFIG['contexts'] = to_config['contexts']
+
+
 def readFiles(curPath):
     """
-    读取
+    读取文件
     :return:
     """
     fileList = os.listdir(curPath)
     for filename in fileList:
         handleConfig(curPath + filename, filename)
+
+
+def handleYaml(c, filename):
+    """
+    处理yaml
+    :param content:  yaml内容
+    :param filename: 文件名称
+    :return:
+    """
+    for i, context in enumerate(c.get('contexts', '')):
+        cluster = context['context']['cluster']
+        user = context['context']['user']
+        name = filename + '-' + str(i)
+        context['name'] = name
+        ALLCONTEXTS.append(name)
+
+        for clu in c.get('clusters', ''):
+            if cluster == clu['name']:
+                cluster_name = name + '-cluster'
+                clu['name'] = cluster_name
+                context['context']['cluster'] = cluster_name
+
+        if len(c.get('clusters')) > len(c.get('users')):
+            user_name = name + '-user'
+            c['users'][0]['name'] = user_name
+            context['context']['user'] = user_name
+        else:
+            for usr in c.get('users', ''):
+                if user == usr['name']:
+                    user_name = name + '-user'
+                    usr['name'] = user_name
+                    context['context']['user'] = user_name
+
+    return c
 
 
 def handleConfig(path, filename):
@@ -82,38 +182,12 @@ def handleConfig(path, filename):
     :param path: 配置文件路径
     :return:
     """
-    cluster = ''
-    user = ''
     with open(path, 'r') as f:
         all = yaml.load(f.read(), Loader=yaml.FullLoader)
-
-        for i, context in enumerate(all.get('contexts', '')):
-            cluster = context['context']['cluster']
-            user = context['context']['user']
-            name = filename + '-' + str(i)
-            context['name'] = name
-            ALLCONTEXTS.append(name)
-
-            for clu in all.get('clusters', ''):
-                if cluster == clu['name']:
-                    cluster_name = name + '-cluster'
-                    clu['name'] = cluster_name
-                    context['context']['cluster'] = cluster_name
-
-            if len(all.get('clusters')) > len(all.get('users')):
-                user_name = name + '-user'
-                all['users'][0]['name'] = user_name
-                context['context']['user'] = user_name
-            else:
-                for usr in all.get('users', ''):
-                    if user == usr['name']:
-                        user_name = name + '-user'
-                        usr['name'] = user_name
-                        context['context']['user'] = user_name
-
-        KUBECONFIG['clusters'].extend(all.get('clusters', ''))
-        KUBECONFIG['users'].extend(all.get('users', ''))
-        KUBECONFIG['contexts'].extend(all.get('contexts', ''))
+        result = handleYaml(all, filename)
+        KUBECONFIG['clusters'].extend(result.get('clusters', ''))
+        KUBECONFIG['users'].extend(result.get('users', ''))
+        KUBECONFIG['contexts'].extend(result.get('contexts', ''))
 
 
 def outputFile():
@@ -130,7 +204,15 @@ def main():
     主方法
     :return:
     """
-    readFiles(getLocalFiles())
+    if FLAGS.add != '' and FLAGS.to == '':
+        raise ToException
+    elif FLAGS.add == '' and FLAGS.to != '':
+        raise AddException
+    elif FLAGS.add != '' and FLAGS.to != '':
+        addConfig()
+        print('开始')
+    else:
+        readFiles(getLocalDirectory())
     outputFile()
     print('# COPY configfile to .kube:')
     print('cp config ~/.kube\n')
@@ -153,5 +235,19 @@ if __name__ == '__main__':
         default="configfile",
         type=str,
         help="the directory kubeconfig!")
+    parser.add_argument(
+        "-t",
+        "--to",
+        dest="to",
+        default="",
+        type=str,
+        help="the kubeconfig that need to join the configured file!")
+    parser.add_argument(
+        "-a",
+        "--add",
+        dest="add",
+        default="",
+        type=str,
+        help="add new config to kubeconfig!")
     FLAGS, unparsed = parser.parse_known_args()
     main()
